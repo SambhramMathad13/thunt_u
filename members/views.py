3b680dc2847025e2389fb71ff7eb5070e2a17ba2
Veteran0411 <somlinghajji@gmail.com>
2024-02-24 20:17:12 +0530

    added validate function and f string to res time

--- members/views.py
+++ members/views.py
@@ -12,6 +12,10 @@ curr_indian_timezone=0
 start_indian_timezone=0
 correct_answer_list=["n","n","n","n","n","n","n","n"]
 
+def validate_text(text):
+    md_text=str(text).lower().strip()
+    return md_text
+
 def login_user(request):
     global start_indian_timezone
     if request.method=="POST":
@@ -36,7 +40,7 @@ def question1(request,name):
     global correct_answer_list
     if request.user.is_authenticated:
         if request.method=="POST":
-            answer=request.POST["answer"]
+            answer=validate_text(request.POST["answer"])
             correct_answer="answer1"
             if answer==correct_answer:
                 correct_answer_list[0]="y"
@@ -54,7 +58,7 @@ def question2(request,name):
     global correct_answer_list
     if request.user.is_authenticated:
         if request.method=="POST":
-            answer=request.POST["answer"]
+            answer=validate_text(request.POST["answer"])
             correct_answer="answer2"
             if answer==correct_answer:
                 correct_answer_list[1]="y"
@@ -77,12 +81,15 @@ def complete(request,name):
 def logout_user(request):
     global curr_indian_timezone
     global correct_answer_list
+    
+    collection_counter=collections.Counter(correct_answer_list)
+    print(collection_counter)
     list=str(correct_answer_list)
     print(list)
     curr=timezone.now()
     curr_indian_timezone=curr.astimezone(pytz.timezone('Asia/Kolkata'))
     time=str(curr_indian_timezone-start_indian_timezone).split(":")
-    restime=" | ".join(time)
+    restime=f"|| {time[0]} hr : {time[1]} min : {time[2]} sec ||"
     print(restime)
     user_name=str(request.user)
     savedata=SubmittedData.objects.create(candidate=user_name,time=restime,list=list)
